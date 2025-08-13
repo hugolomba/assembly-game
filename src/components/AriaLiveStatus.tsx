12847ae79157f590ee1780acc8ce0fb52ea53acf
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-08-14 00:13:15 +0100

    add LanguageChips component

--- src/components/AriaLiveStatus.tsx
+++ src/components/AriaLiveStatus.tsx
@@ -1,92 +1,37 @@
-import {useState} from "react"
-import {languages} from "./languages"
-import {getRandomWord} from "./utils"
 
-import ConfettiContainer from "./components/ConfettiContainer"
-import Header from './components/Header'
-import GameStatus from "./components/GameStatus";
-import LanguageChips from "./components/LanguageChips";
-import WordLetters from "./components/WordLetters";
-import AriaLiveStatus from "./components/AriaLiveStatus";
-import Keyboard from "./components/Keyboard";
-import NewGameButton from "./components/NewGameButton";
+import type {JSX} from 'react'
 
-export default function AssemblyEndgame() {
-    // State values
-    const [currentWord, setCurrentWord] = useState<string>(():string => getRandomWord())
-    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+type AriaLiveStatusProps = {
+    currentWord:string,
+    lastGuessedLetter:string,
+    guessedLetters:string[],
+    numGuessesLeft:number
+}
 
-
-    // Derived values
-    const numGuessesLeft:number = languages.length - 1
-    const wrongGuessCount:number =
-        guessedLetters.filter((letter:string):boolean => !currentWord.includes(letter)).length
-    const isGameWon:boolean =
-        currentWord.split("").every((letter:string):boolean => guessedLetters.includes(letter))
-    const isGameLost:boolean = wrongGuessCount >= numGuessesLeft
-    const isGameOver:boolean = isGameWon || isGameLost
-    const lastGuessedLetter:string = guessedLetters[guessedLetters.length - 1]
-    const isLastGuessIncorrect:boolean = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
-
-    // Static values
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    function addGuessedLetter(letter:string):void {
-        setGuessedLetters((prevLetters:string[]):string[] =>
-            prevLetters.includes(letter) ?
-                prevLetters :
-                [...prevLetters, letter]
-        )
-    }
-
-    function startNewGame():void {
-        setCurrentWord(getRandomWord())
-        setGuessedLetters([])
-    }
-
+export default function AriaLiveStatus({
+                                           currentWord,
+                                           lastGuessedLetter,
+                                           guessedLetters,
+                                           numGuessesLeft
+                                       }:AriaLiveStatusProps):JSX.Element {
     return (
-        <main>
-            <ConfettiContainer isGameWon={isGameWon}/>
-            <Header/>
-
-            <GameStatus
-                isGameWon={isGameWon}
-                isGameLost={isGameLost}
-                isGameOver={isGameOver}
-                isLastGuessIncorrect={isLastGuessIncorrect}
-                wrongGuessCount={wrongGuessCount}
-            />
-
-            <LanguageChips
-                languages={languages}
-                wrongGuessCount={wrongGuessCount}
-            />
-
-            <WordLetters
-                currentWord={currentWord}
-                guessedLetters={guessedLetters}
-                isGameLost={isGameLost}
-            />
-
-            <AriaLiveStatus
-                currentWord={currentWord}
-                lastGuessedLetter={lastGuessedLetter}
-                guessedLetters={guessedLetters}
-                numGuessesLeft={numGuessesLeft}
-            />
-
-            <Keyboard
-                alphabet={alphabet}
-                guessedLetters={guessedLetters}
-                currentWord={currentWord}
-                isGameOver={isGameOver}
-                addGuessedLetter={addGuessedLetter}
-            />
-
-            <NewGameButton
-                isGameOver={isGameOver}
-                startNewGame={startNewGame}
-            />
-        </main>
+        <section
+            className="sr-only"
+            aria-live="polite"
+            role="status"
+        >
+            <p>
+                {currentWord.includes(lastGuessedLetter) ?
+                    `Correct! The letter ${lastGuessedLetter} is in the word.` :
+                    `Sorry, the letter ${lastGuessedLetter} is not in the word.`
+                }
+                You have {numGuessesLeft} attempts left.
+            </p>
+            <p>
+                Current word: {currentWord.split("").map((letter:string):string =>
+                guessedLetters.includes(letter) ? letter + "." : "blank."
+            ).join(" ")}
+            </p>
+        </section>
     )
 }
---
+++ src/components/LanguageChips.tsx
@@ -0,0 +1,37 @@
+/*
+CHALLENGE 1: Somehow access the Language type in
+             `languages.ts` from `LanguageChips.tsx`
+CHALLENGE 2: Use it to type the languages prop
+CHALLENGE 3: Type the remaining props
+*/
+
+import { clsx } from "clsx"
+import type {JSX} from 'react'
+import type {Language} from "../languages"
+
+type LanguageChipsProps = {
+    languages: Language[],
+    wrongGuessCount: number
+}
+
+export default function LanguageChips({ languages, wrongGuessCount }:LanguageChipsProps):JSX.Element {
+    const languageElements = languages.map((lang, index) => {
+        const isLanguageLost = index < wrongGuessCount
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        const className = clsx("chip", isLanguageLost && "lost")
+        return (
+            <span
+                className={className}
+                style={styles}
+                key={lang.name}
+            >
+        {lang.name}
+      </span>
+        )
+    })
+
+    return <section className="language-chips">{languageElements}</section>
+}
