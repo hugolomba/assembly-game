cc66fea1fd8cd1f49f172f114f9bdc43dd3b4fc7
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-08-14 12:22:38 +0100

    fix type error in IsLastGuessIncorrect variable

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ export default function AssemblyEndgame() {
     const isGameLost:boolean = wrongGuessCount >= numGuessesLeft
     const isGameOver:boolean = isGameWon || isGameLost
     const lastGuessedLetter:string = guessedLetters[guessedLetters.length - 1]
-    const isLastGuessIncorrect:string | boolean = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
 
     // Static values
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
